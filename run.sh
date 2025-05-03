77ce53480168c76625e551e92b010198c1c4b7f0
jative <jativeofficial@gmail.com>
2025-05-03 13:59:17 +0500

    geändert:       readme.md 	geändert:       requirements.txt 	neue Datei:     run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! systemctl is-active --quiet mariadb; then
+    echo "MariaDB не запущена. Запускаем службу..."
+    sudo systemctl start mariadb
+    if systemctl is-active --quiet mariadb; then
+        echo "MariaDB успешно запущена"
+    else
+        echo "Ошибка! Не удалось запустить MariaDB" >&2
+        exit 1
+    fi
+else
+    echo "MariaDB уже работает"
+fi
+
+konsole --noclose -e "bash -c 'source ./venv/bin/activate && python ./servers/web-server/app.py; exec bash'" &
+konsole --noclose -e "bash -c 'source ./venv/bin/activate && python ./servers/IoT-server/main.py; exec bash'" &
+konsole --noclose -e "bash -c 'source ./venv/bin/activate && python ./bots/main.py; exec bash'" &
+
+exit
